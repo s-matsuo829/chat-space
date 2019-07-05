9dd71c5a89f8ef3d28032acf4fd7712324742f37
s-matsu829 <fakefur829@gmail.com>
2019-07-05 12:45:33 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,9 @@
 class MessagesController < ApplicationController
-  before_action :set_group, :set_time
+  before_action :set_group, :set_time, :set_members
 
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
-    @members = @group.users
   end
 
   def create
