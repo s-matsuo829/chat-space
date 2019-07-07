04ea17d2c039f3cf31f8709a55e43c3e7ef8395d
s-matsu829 <fakefur829@gmail.com>
2019-07-07 15:28:33 +0900

    edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,6 +19,7 @@ class GroupsController < ApplicationController
   end
 
   def update
+    # binding.pry
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
