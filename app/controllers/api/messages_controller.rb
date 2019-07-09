eaafd0110f29e90d486b4a270fd5c0abddb57e2e
s-matsu829 <fakefur829@gmail.com>
2019-07-09 13:50:10 +0900

    edit

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,16 @@
 class Api::MessagesController < ApplicationController
+  before_action :set_time
+
   def index
     @group = Group.find(params[:group_id]) 
     @messages = @group.messages.includes(:user).where('id > ?', params[:id]) 
+  end
+
+
+  private
+  def set_time
+    wd = ["Sun", "Mon", "tue", "Wed", "Thu", "Fri", "Sat"]
+    time = Time.now
+    @time = wd[time.wday]
   end
 end
